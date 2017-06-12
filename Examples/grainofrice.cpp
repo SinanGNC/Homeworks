31062dadfd040b6c671a46ca755ed2a49994b0dd
SinanGNC <sinan.gencoglu@hotmail.com>
2017-06-12 22:52:36 +0300

    Update grainofrice.cpp

--- Examples/grainofrice.cpp
+++ Examples/grainofrice.cpp
@@ -1,1 +1,24 @@
 
+/*Write a program to calculate how many squares are required to give the inventor at least X grains of rice,
+ i.e. X=1000 or X=1,000,000 grains, etc. Your program will print the number of squares to the screen.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+    int input,sum=0,k=0,i;
+    
+    cin >> input;
+    
+    if (input != -1) {
+    for (i=1; sum<input; i*=2) {
+        sum += i;
+        k++;
+     }
+    }
+    cout << k << endl;
+    
+    return 0;
+}

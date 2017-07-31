10e38583c6feec2fd3a03ddea4c0556201d675ad
SinanGNC <sinan.gencoglu@hotmail.com>
2017-07-31 20:33:20 +0300

    Update heightoftheball.cpp

--- Examples/heightoftheball.cpp
+++ Examples/heightoftheball.cpp
@@ -1,3 +1,11 @@
+/*  Write a short program to simulate a ball being dropped off of a tower.
+To start, the user should be asked for the initial height of the tower in meters.
+Assume normal gravity (9.8 m/s2), and that the ball has no initial velocity (the ball is not moving to start).
+Have the program output the height of the ball above the ground after 0, 1, 2, 3, 4, and 5 seconds.
+The ball should not go underneath the ground (height 0).
+Your program should include a header file named constants.h that includes a namespace called myConstants.
+In the myConstants namespace, define a symbolic constant to hold the value of gravity (9.8). */
+
 //constant.h
 
 #ifndef CONSTANT_H
